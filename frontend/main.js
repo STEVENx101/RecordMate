61c121b58c6fc2ec21edc1cc432a9a08fc8a01c5
DilushaFdo <130301311+DilushaFdo@users.noreply.github.com>
2024-02-27 14:18:23 +0530

    Changed the program to be automated using nodemon
    No need to run the program from the terminal all the changes can be see while running with nodemon

--- frontend/main.js
+++ frontend/main.js
@@ -1,21 +1,8 @@
-<<<<<<< Updated upstream
-const {BrowserWindow, app} = require('electron');
-=======
 const { BrowserWindow, app } = require('electron');
->>>>>>> Stashed changes
 
 const createWindow = () => {
     const loginWin = new BrowserWindow({
         width: 800,
-<<<<<<< Updated upstream
-        height: 600
-    })
-
-    win.loadFile("index.html")
-}
-
-
-=======
         height: 600,
         maximizable: false,
        
@@ -24,17 +11,11 @@ const createWindow = () => {
     loginWin.setMenu(null)
     loginWin.loadFile("Loginpage.html");
 };
->>>>>>> Stashed changes
 
 app.whenReady().then(() => {
     createWindow();
 });
 
 app.on('window-all-closed', () => {
-<<<<<<< Updated upstream
-    if (process.platform !== 'darwin') app.quit()
-})
-=======
     if (process.platform !== 'darwin') app.quit();
 });
->>>>>>> Stashed changes
