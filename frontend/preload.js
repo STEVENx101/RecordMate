f50209bc1c3954edbc5fadb6db133793252dfa27
DilushaFdo <130301311+DilushaFdo@users.noreply.github.com>
2024-03-08 18:29:22 +0530

    added close button

--- frontend/preload.js
+++ frontend/preload.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     const loginbtn = document.getElementById('loginBtn');
     const logoutbtn = document.getElementById('logoutBtn');
     const recordWindowbtn = document.getElementById('recordBtn');
+    const closeRecordBtn = document.getElementById('closeRecordBtn');
     
     if(loginbtn){
         loginbtn.addEventListener('click', () => {
@@ -21,6 +22,13 @@ document.addEventListener('DOMContentLoaded', () =>{
     if(recordWindowbtn){
         recordWindowbtn.addEventListener('click', () => {
             ipcRenderer.send('open-record-window');
+        });
+    }
+
+    if (closeRecordBtn) {
+        closeRecordBtn.addEventListener('click', () => {
+            // Send a message to the main process to close the recordWindow
+            ipcRenderer.send('close-record-window');
         });
     }
     
